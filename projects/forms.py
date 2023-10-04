5bc2bea915f265a38f654dad592c32bb766abae8
Ashish Kushwaha <therobomarket@gmail.com>
2023-10-04 11:19:32 +0530

    Themes Added

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
@@ -9,3 +10,15 @@ class ProjectForm(ModelForm):
         # fields = '__all__'
         fields = ['title', 'description', 'featured_image', 'demo_link', 
                   'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+        
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        
+        # self.fields['title'].widget.attrs.update({'class': 'input', 
+        #                                           'placeholder': 'Add Title'})
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input', 'placeholder': f"Add {name}"})
