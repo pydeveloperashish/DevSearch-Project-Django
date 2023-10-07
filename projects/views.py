d36a3eda08807b18a100bd4b9604afe1fa70eaf2
Ashish Kushwaha <therobomarket@gmail.com>
2023-10-07 22:26:34 +0530

    login_required function added

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
+
 
 # Create your views here.
 
@@ -23,7 +25,8 @@ def project(request, pk):
     return render(request, 'projects/single-project.html', 
                   {'projectObj': projectObj})
     
-    
+
+@login_required(login_url = "login")
 def createProject(request):
     form = ProjectForm()
     if request.method == 'POST':
@@ -37,6 +40,7 @@ def createProject(request):
     return render(request, 'projects/project_form.html', context)
 
 
+@login_required(login_url = "login")
 def updateProject(request, pk):
     project = Project.objects.get(id = pk)
     form = ProjectForm(instance = project)
@@ -51,6 +55,7 @@ def updateProject(request, pk):
     return render(request, 'projects/project_form.html', context)
 
 
+@login_required(login_url = "login")
 def deleteProject(request, pk):
     project = Project.objects.get(id = pk)
     if request.method == 'POST':
