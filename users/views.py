a1cf750134d115b0de6179c91bb536155475f354
Ashish Kushwaha <therobomarket@gmail.com>
2023-10-14 10:00:41 +0530

    searching through skills added

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 #from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Skill
 from django.db.models import Q
 
 # Create your views here.
@@ -66,6 +66,8 @@ def profiles(request):
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
     print('SEARCH QUERY', search_query)
+    
+    skills = Skill.objects.filter(name__iexact = search_query)
 
     # If we use filter like this, then when we search for profiles, the name,
     # and short_intro, both needs to contain the same search query,
@@ -73,9 +75,11 @@ def profiles(request):
     # profiles = Profile.objects.filter(name__icontains = search_query,
     #                                   short_intro__icontains = search_query)
 
-    # so we gonna use Q lookups
-    profiles = Profile.objects.filter(Q(name__icontains = search_query) |
-                                      Q(short_intro__icontains = search_query))
+    # so we gonna use Q lookups to prevent what's happening above.
+    profiles = Profile.objects.distinct().filter(Q(name__icontains = search_query) |
+                                      Q(short_intro__icontains = search_query) |
+                                      Q(skill__in = skills)
+                                      )
     context = {"profiles": profiles, 'search_query': search_query}
     return render(request, 'users/profiles.html', context)
 
