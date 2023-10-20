883fc8479b5c0f52980f561b28a970fa980061de
Ashish Kushwaha <therobomarket@gmail.com>
2023-10-20 20:32:03 +0530

    templates added for password reset process

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -24,14 +24,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), 
-                name = 'reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
-                name = 'password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(),
-                name = 'password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
-                name = 'password_reset_complete'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name = 'reset_password.html'), 
+        name = 'reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name = 'reset_password_sent.html'),
+        name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+        template_name = 'reset.html'),
+        name = 'password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name = 'reset_password_complete.html'),
+        name = 'password_reset_complete'),
 ]
 
 # MEDIA_ROOT:- where to store images
