38a80a37bce7dbc9711fd654b3309100517f6378
Ashish Kushwaha <therobomarket@gmail.com>
2023-10-28 10:02:41 +0530

    pagination bug fixed

--- users/models.py
+++ users/models.py
@@ -28,6 +28,9 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.username)
     
+    class Meta:
+        ordering = ['created']  
+    
     
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete = models.CASCADE,
